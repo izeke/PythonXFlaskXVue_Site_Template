81a7505257cd9e256b949d45dc59a8ebf777e6d7
Emma Paris <34948409+izeke@users.noreply.github.com>
2019-11-11 12:11:04 -0600

    Revert "Added resulting image display and manipulation buttons"
    This reverts commit 8b829845cf20d92002633d649730322ea5315034.
    
    Commits should have been committed to the CSCI2620_site repo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, json, request, url_for
+from flask import Flask, render_template, json, request
 from pprint import pprint
 from os import listdir
-import cv2
-import glitch
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -34,70 +32,6 @@ def testalbums():
 @app.route('/getInputImageFilenames')
 def getInputImageFilenames():
     return json.dumps(listdir(app.root_path + "/static/img/input"))
-
-@app.route('/loadImage', methods=['POST'])
-def loadImage():
-    imgUrl = url_for('static', filename="img/input/" + request.json['params']['image'])[1:]
-    img = cv2.imread(imgUrl, -1)
-    cv2.imwrite(url_for('static', filename="img/")[1:] + "out.jpg", img)
-    return json.dumps(True)
-
-@app.route('/addVLine')
-def addVLine():
-    img = cv2.imread(url_for('static', filename="img/out.jpg")[1:], -1)
-    img = glitch.addVLines(img, 5, 200, 1)
-    cv2.imwrite(url_for('static', filename="img/")[1:] + "out.jpg", img)
-    return json.dumps(True)
-
-@app.route('/addHLine')
-def addHLine():
-    img = cv2.imread(url_for('static', filename="img/out.jpg")[1:], -1)
-    img = glitch.addHLines(img, 5, 200, 1)
-    cv2.imwrite(url_for('static', filename="img/")[1:] + "out.jpg", img)
-    return json.dumps(True)
-
-@app.route('/shiftSubsetV')
-def shiftSubsetV():
-    img = cv2.imread(url_for('static', filename="img/out.jpg")[1:], -1)
-    img = glitch.shiftSubsetV(img, 5, 200, 1)
-    cv2.imwrite(url_for('static', filename="img/")[1:] + "out.jpg", img)
-    return json.dumps(True)
-
-@app.route('/shiftSubsetH')
-def shiftSubsetH():
-    img = cv2.imread(url_for('static', filename="img/out.jpg")[1:], -1)
-    img = glitch.shiftSubsetH(img, 5, 200, 1)
-    cv2.imwrite(url_for('static', filename="img/")[1:] + "out.jpg", img)
-    return json.dumps(True)
-
-@app.route('/fuzzify')
-def fuzzify():
-    img = cv2.imread(url_for('static', filename="img/out.jpg")[1:], -1)
-    img = glitch.fuzzify(img)
-    cv2.imwrite(url_for('static', filename="img/")[1:] + "out.jpg", img)
-    return json.dumps(True)
-
-@app.route('/scanlineify')
-def scanlineify():
-    img = cv2.imread(url_for('static', filename="img/out.jpg")[1:], -1)
-    img = glitch.scanlineify(img, 4)
-    cv2.imwrite(url_for('static', filename="img/")[1:] + "out.jpg", img)
-    return json.dumps(True)
-
-@app.route('/bitify')
-def bitify():
-    img = cv2.imread(url_for('static', filename="img/out.jpg")[1:], -1)
-    img = glitch.bitify(img, 4)
-    cv2.imwrite(url_for('static', filename="img/")[1:] + "out.jpg", img)
-    return json.dumps(True)
-
-@app.route('/splitColors')
-def splitColors():
-    img = cv2.imread(url_for('static', filename="img/out.jpg")[1:], -1)
-    img = glitch.shiftBlue(img, 10, 10)
-    img = glitch.shiftRed(img, -10, -10)
-    cv2.imwrite(url_for('static', filename="img/")[1:] + "out.jpg", img)
-    return json.dumps(True)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/app.vue.js
+++ static/js/app.vue.js
@@ -4,8 +4,7 @@ var app = new Vue({
     data: {
         band: {},
         albums: {},
-        images: {},
-        loadedImage: "static/img/placeholder.jpg"
+        images: {}
     },
     methods: {
         fetchData: function() {
@@ -24,95 +23,6 @@ var app = new Vue({
 
             this.$http.get('/getInputImageFilenames').then(response => {
                 this.images = response.body;
-            })
-        },
-        loadImage: function(image) {
-            this.$http.post('/loadImage', {params: {'image': image}}, {headers: {'Cache-Control': 'max-age=0, must-revalidate, no-store'}}).then(response => {
-                let newImage = new Image();
-                newImage.src = "static/img/out.jpg?" + new Date().getTime();
-                this.loadedImage = newImage.src;
-            }, response => {
-                console.log("an error occurred");
-            });
-        },
-        addVLine: function() {
-            this.$http.get('/addVLine').then(response => {
-                let newImage = new Image();
-                newImage.src = "static/img/out.jpg?" + new Date().getTime();
-                this.loadedImage = newImage.src;
-                console.log("successfully updated image")
-            }, response => {
-                console.log("an error occurred");
-            })
-        },
-        addHLine: function() {
-            this.$http.get('/addHLine').then(response => {
-                let newImage = new Image();
-                newImage.src = "static/img/out.jpg?" + new Date().getTime();
-                this.loadedImage = newImage.src;
-                console.log("successfully updated image")
-            }, response => {
-                console.log("an error occurred");
-            })
-        },
-        shiftSubsetV: function() {
-            this.$http.get('/shiftSubsetV').then(response => {
-                let newImage = new Image();
-                newImage.src = "static/img/out.jpg?" + new Date().getTime();
-                this.loadedImage = newImage.src;
-                console.log("successfully updated image")
-            }, response => {
-                console.log("an error occurred");
-            })
-        },
-        shiftSubsetH: function() {
-            this.$http.get('/shiftSubsetH').then(response => {
-                let newImage = new Image();
-                newImage.src = "static/img/out.jpg?" + new Date().getTime();
-                this.loadedImage = newImage.src;
-                console.log("successfully updated image")
-            }, response => {
-                console.log("an error occurred");
-            })
-        },
-        fuzzify: function() {
-            this.$http.get('/fuzzify').then(response => {
-                let newImage = new Image();
-                newImage.src = "static/img/out.jpg?" + new Date().getTime();
-                this.loadedImage = newImage.src;
-                console.log("successfully updated image")
-            }, response => {
-                console.log("an error occurred");
-            })
-        },
-        scanlineify: function() {
-            this.$http.get('/scanlineify').then(response => {
-                let newImage = new Image();
-                newImage.src = "static/img/out.jpg?" + new Date().getTime();
-                this.loadedImage = newImage.src;
-                console.log("successfully updated image")
-            }, response => {
-                console.log("an error occurred");
-            })
-        },
-        bitify: function() {
-            this.$http.get('/bitify').then(response => {
-                let newImage = new Image();
-                newImage.src = "static/img/out.jpg?" + new Date().getTime();
-                this.loadedImage = newImage.src;
-                console.log("successfully updated image")
-            }, response => {
-                console.log("an error occurred");
-            })
-        },
-        splitColors: function() {
-            this.$http.get('/splitColors').then(response => {
-                let newImage = new Image();
-                newImage.src = "static/img/out.jpg?" + new Date().getTime();
-                this.loadedImage = newImage.src;
-                console.log("successfully updated image")
-            }, response => {
-                console.log("an error occurred");
             })
         }
     },
